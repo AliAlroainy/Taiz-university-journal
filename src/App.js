6f8a1f98aa8efd761de8b08d22cafec02e58f10d
Ali <aliabduh.2020@gmail.com>
2021-01-11 13:15:53 +0300

    all footers with the top header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
-import logo from './T.svg';
-import Hi from './hi'
-//import Said from './main/said';
-import Header from './main/header';
-import { BrowserRouter , Link, Route  } from 'react-router-dom';
-import Bye from './bye';
-import Footer from './main/footer'
-
-
-
-// import nav pages 
-import Editorial from './pages/nav/Editorial';
-import AuthorGuideline from './pages/nav/AuthorGuideline';
-import PaperSubmission from './pages/nav/PaperSubmission';
-import Home from './pages/Home';
-import Said from './main/said';
+import Header from './components/Topheader/header';
+import Footer from './components/footer/footer'
+import Bottomfooter from './components/footer/bottomFooter';
 
 
 function App() {
@@ -23,9 +10,8 @@ function App() {
        
     <div  >
        <Header/>
-       <Said/>
        <Footer/>
-    
+       <Bottomfooter/>
     </div>
  
 
---
+++ src/components/footer/bottomFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './bottomFooter.css'
+
+const Bottomfooter = () => {
+    return (
+        <div className="f">
+           Powerd by Taiz University technical Team 
+        </div>
+    )
+}
+
+export default Bottomfooter;
