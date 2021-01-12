1b3777e28d018d054182ab6bcdf831b4c77978ce
Ali <aliabduh.2020@gmail.com>
2021-01-12 03:39:37 +0300

    ohlala

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import UpperHeader from './components/Topheader/header';
 import Footer from './components/footer/footer'
 import Bottomfooter from './components/footer/bottomFooter';
 import MainHeader from './components/Header';
+import Home from './pages/home/Home';
 import { BrowserRouter } from "react-router-dom";
+import Newfooter from './components/newfooter';
 
 
 function App() {
@@ -12,8 +14,8 @@ function App() {
     <div  >
        <UpperHeader/>
        <MainHeader/>
-       <Footer/>
-       <Bottomfooter/>
+       <Home/>
+      <Newfooter/>
     </div>
   </BrowserRouter>       
   );
---
+++ src/pages/home/latestpapers/latest.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './latest.css'
+ const Latest = () => {
+    return (
+            <>
+            <p className="text-center mt-10 text-gray-800 text-3xl font-semibold">The latest reseaches</p>
+        <div className="flex flex-wrap" >
+             
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+            
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+            
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+            <div className="max-w-sm py-4 px-8 bg-white shadow-md rounded-lg mx-10 my-10">
+                 <div>
+                <h2 className="text-gray-800 text-3xl font-semibold">``Design Tools,,</h2>
+                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                </div>
+                  <div className="flex justify-end mt-4">
+                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+                  </div>
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default Latest;
